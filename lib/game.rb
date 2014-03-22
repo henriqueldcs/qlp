871b49d143353a4ddb4cd505485925a1e22bbf02
henriqueldcs <henriqueldcs@gmail.com>
2014-03-22 15:56:11 -0300

    TASK1 - Caso de testes game Game#kill

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,27 @@
 class Game
 	
-	attr_reader :players
+	attr_reader :players, :total_kills
 	
 	def initialize
 		@players = Hash.new
+		@total_kills = 0
 	end
 
 	def addPlayer(player)
 		@players [player.id] = player
 	end
+  
+  def kill(killer, killed)
+    
+    @total_kills += 1
+    
+    if( killer == :world )
+      
+      killed.suicide
+    else
+      killer.kill
+      killed.die
+    end
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,24 +3,69 @@ require 'player'
 require 'game'
 
 describe Game do
+  
+  before do
+  
+    @game = Game.new
+    @player1 = Player.new 1
+    @player2 = Player.new 2
+  end
+  
 	describe "#addPlayer" do
 		it "Adiciona um jogador ao jogo" do
-			player = Player.new 1
-			game = Game.new
-			game.addPlayer(player)
+			
+			@game.addPlayer(@player1)
 
-			expect(game.players.size).to eq(1)
+			expect(@game.players.size).to eq(1)
 		end
 
 		it "Adiciona dois jogadores ao jogo" do
-			player1 = Player.new 1
-			player2 = Player.new 2
-			game = Game.new
-			game.addPlayer(player1)
-			game.addPlayer(player2)
 
-			expect(game.players.size).to eq(2)
+			@game = Game.new
+			@game.addPlayer(@player1)
+			@game.addPlayer(@player2)
+
+			expect(@game.players.size).to eq(2)
 		end
+	end
+	
+	describe "#kill" do
+	  it "Quando o <world> mata o player ele perde -1 kill." do
+	    
+	    @game.addPlayer(@player1)
+	    @game.kill(:world, @player1)
+	    
+	    expect( @player1.kills ).to eq(-1)
+	  end
+	
+  	it "Quando um player mata um adversário seu atributo kills deve ser incrementado." do
+  	  @game.addPlayer(@player1)
+      @game.addPlayer(@player2)
+      
+      @game.kill(@player1, @player2)
+      
+      expect( @player1.kills).to eq(1)
+  	end
+  	
+  	it "Quando um player é morto por um adversário seu atributo deaths deve ser incrementado." do
+      @game.addPlayer(@player1)
+      @game.addPlayer(@player2)
+      
+      @game.kill(@player1, @player2)
+      
+      expect( @player2.deaths).to eq(1)
+    end
+    
+    it "Quando ocorre uma morte no jogo, total_kills deve ser incrementado." do
+      
+      @game.addPlayer(@player1)
+      @game.addPlayer(@player2)
+      
+      @game.kill(@player1, @player2)
+      @game.kill(:world, @player2)
+      
+      expect(@game.total_kills).to eq(2)
+    end
 	end
 	
 end
